f9678b7dbc21e3fe47e83352b8db9d0dfb8110d8
rlaehgus9847 <rlaehgus9847@office.uos.ac.kr>
2022-09-27 17:10:06 +0900

    3

--- 20220927LinkedList/20220927LinkedList.cpp
+++ 20220927LinkedList/20220927LinkedList.cpp
@@ -15,17 +15,19 @@ typedef struct node {
 typedef int myType; //앞으로 myType이라는 자료형은 int형이다.
 int main() {
 	//struct node a; //구조체 변수
-	Node a;
+	//Node a;
 	//struct node* pNode; //구조체 포인터 변수
-	Node* pNode;
+	Node* pNode; //stack 영역
 	myType n;// -> int n
-	a.data = 10;//a의 멤버 변수 data에 다시 접근해 10으로 초기화
-	a.pNext = NULL;
+	static int a;
+	pNode = (Node *)malloc(sizeof(Node)); // malloc(원하는 Byte 수) heap 영역
+	pNode->data = 10; //멤버접근 heap 영역
+	pNode->pNext = NULL; //멤버접근 heap 영역
+	//a.data = 10;//a의 멤버 변수 data에 다시 접근해 10으로 초기화
+	//a.pNext = NULL;
 
-	pNode = &a; //&a = address of a
+	//pNode = &a; //&a = address of a
 	printf("%d\n", pNode->data);//a의 멤버 변수 data를 접근하는 또다른 방법
 
-
-	
 	return 0;
 }

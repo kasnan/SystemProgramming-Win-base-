ec0c972fb41b477f442a77b3845eac6f0f115138
rlaehgus9847 <rlaehgus9847@office.uos.ac.kr>
2022-09-27 16:38:22 +0900

    2

--- 20220927LinkedList/20220927LinkedList.cpp
+++ 20220927LinkedList/20220927LinkedList.cpp
@@ -1,16 +1,30 @@
 ﻿#include<stdio.h>
 #include<stdlib.h>
 
+//struct node {
+//	int data;//data filed, 멤버변수
+//	struct node* pNext;//link field, 
+//} ;
 
-struct node {
+typedef struct node {
 	int data;//data filed, 멤버변수
 	struct node* pNext;//link field, 
-};
+} Node;
 
+//typedef struct node Node;//앞으로 Node는 struct node이다.
+typedef int myType; //앞으로 myType이라는 자료형은 int형이다.
 int main() {
-	struct node a;
-	a.data = 10;
+	//struct node a; //구조체 변수
+	Node a;
+	//struct node* pNode; //구조체 포인터 변수
+	Node* pNode;
+	myType n;// -> int n
+	a.data = 10;//a의 멤버 변수 data에 다시 접근해 10으로 초기화
 	a.pNext = NULL;
+
+	pNode = &a; //&a = address of a
+	printf("%d\n", pNode->data);//a의 멤버 변수 data를 접근하는 또다른 방법
+
 
 	
 	return 0;
